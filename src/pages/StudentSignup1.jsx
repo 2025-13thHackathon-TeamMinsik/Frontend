aa2d45a6b08e99ff1cee2882ae60d3bbb3de8da3
twolim <happygrove8@gmail.com>
2025-08-21 02:31:46 +0900

    [feat] 로그인 연동

--- src/pages/StudentSignup1.jsx
+++ src/pages/StudentSignup1.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { useState } from "react";
 import axios from "axios";
 import * as S from "../styles/StyledStudentSignup";
 
 const StudentSignup1 = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [selected, setSelected] = useState("동덕여자대학교");
+
+  const sorts = [
+    "고려대학교",
+    "국민대학교",
+    "한성대학교",
+    "동덕여자대학교",
+    "성신여자대학교",
+    "서경대학교",
+    "한국예술종합학교",
+  ];
+
+  //토글 아이콘
+  const handleToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleSelected = (sort) => {
+    setSelected(sort); //sort선택하면
+    setIsMenuOpen(false); //메뉴 닫기
+  };
+
   return (
     <S.Container>
       <S.Bar>
@@ -20,13 +44,29 @@ const StudentSignup1 = () => {
         />
       </S.Background2>
       <S.Title>어느 학교에 다니고 있나요?</S.Title>
-      <S.SchoolInput type="text"></S.SchoolInput>
-      <S.Toggle>
-        <img
-          src={`${process.env.PUBLIC_URL}/images/toggleDown.svg`}
-          alt="toggleDown"
-        />
-      </S.Toggle>
+      <div id="SchoolBox">
+        <S.SchoolInput onClick={handleToggle}>
+          {selected}
+          <img
+            src={`${process.env.PUBLIC_URL}/images/${
+              isMenuOpen ? "toggleUp.svg" : "toggleDown.svg"
+            }`}
+            alt="toggleIcon"
+            width="16px"
+            height="auto"
+            id="toggleIcon"
+          />
+        </S.SchoolInput>
+        {isMenuOpen && (
+          <S.FilterToggle>
+            {sorts.map((item) => (
+              <div key={item} onClick={() => handleSelected(item)}>
+                {item}
+              </div>
+            ))}
+          </S.FilterToggle>
+        )}
+      </div>
       <S.BackBtn>
         <img
           src={`${process.env.PUBLIC_URL}/images/backBtn.svg`}
